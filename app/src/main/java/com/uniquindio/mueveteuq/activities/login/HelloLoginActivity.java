6f6cbfb83cb6a3000bedc6f84b562245f70f0306
MariaPaulaS <mariathecharmix@hotmail.com>
2020-01-23 11:58:11 -0500

    Validación de permisos arreglada

--- app/src/main/java/com/uniquindio/mueveteuq/activities/login/HelloLoginActivity.java
+++ app/src/main/java/com/uniquindio/mueveteuq/activities/login/HelloLoginActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.uniquindio.mueveteuq.R;
+import com.uniquindio.mueveteuq.util.Utilities;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -25,6 +26,7 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 /**
  * Actividad que recibe al usuario y le da la opcion de crear cuenta o registrarse
  * Configurar para opcion de invitado
+ * @author MariaTheCharmix
  */
 public class HelloLoginActivity extends AppCompatActivity {
 
@@ -37,8 +39,7 @@ public class HelloLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_login);
 
-        validarPermisos();
-
+    //    Utilities.validarPermisos(this, HelloLoginActivity.this);
 
         getSupportActionBar().hide();
 
@@ -69,33 +70,15 @@ public class HelloLoginActivity extends AppCompatActivity {
     }
 
 
-    /**
-     * Método que verifica si la app tiene permisos para funcionar. Si no, los pide al momento de
-     * la instalación.
-     * Solo se piden una vez.
-     * Funciona desde Android Malvavisco en adelante.
-     */
-    private void validarPermisos() {
-
-        //Para version 5.
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return;
-        }
-
-        if (ContextCompat.checkSelfPermission(HelloLoginActivity.this, WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(HelloLoginActivity.this, CAMERA)
-                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(HelloLoginActivity.this, ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(HelloLoginActivity.this, ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-
-            ActivityCompat.requestPermissions(HelloLoginActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION, CAMERA, WRITE_EXTERNAL_STORAGE}, 1);
 
 
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Validar permisos de la app
+        Utilities.validarPermisos(this, HelloLoginActivity.this);
+
+
     }
-
-
-
 }
