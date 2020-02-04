1da70bcad38983cda434fb2f7a979b7028e8e80b
MariaPaulaS <mariathecharmix@hotmail.com>
2020-02-04 16:11:27 -0500

    Creada vista de resumen de resultados

---
+++ app/src/main/java/com/uniquindio/mueveteuq/activities/ResultRaceActivity.java
@@ -0,0 +1,16 @@
+package com.uniquindio.mueveteuq.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uniquindio.mueveteuq.R;
+
+public class ResultRaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_race);
+    }
+}
