8ff7d2993685dc05ef57bafbf20705b3ec665687
MariaPaulaS <mariathecharmix@hotmail.com>
2020-06-01 21:08:52 -0500

    Fix de iconos de usuario

--- app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/LightRewardFragment.java
+++ app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/LightRewardFragment.java
@@ -2,11 +2,13 @@ package com.uniquindio.mueveteuq.fragments.mainZone;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.uniquindio.mueveteuq.R;
 
@@ -24,6 +26,8 @@ public class LightRewardFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private CardView cardViewManzana;
 
     public LightRewardFragment() {
         // Required empty public constructor
@@ -60,6 +64,19 @@ public class LightRewardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_light_reward, container, false);
+        View view = inflater.inflate(R.layout.fragment_light_reward, container, false);
+
+
+        cardViewManzana = view.findViewById(R.id.cardManzana);
+
+        cardViewManzana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Funciona", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        return view;
     }
 }
--- app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/RewardsFragment.java
+++ app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/RewardsFragment.java
@@ -2,11 +2,13 @@ package com.uniquindio.mueveteuq.fragments.mainZone;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.uniquindio.mueveteuq.R;
 
@@ -24,6 +26,8 @@ public class RewardsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
 
     public RewardsFragment() {
         // Required empty public constructor
@@ -60,6 +64,9 @@ public class RewardsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rewards, container, false);
+        View view = inflater.inflate(R.layout.fragment_rewards, container, false);
+
+
+        return view;
     }
 }
