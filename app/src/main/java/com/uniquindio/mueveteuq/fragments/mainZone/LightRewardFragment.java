6e4d26d361cc02d1d9f2bcd9784b87097dba87f3
MariaPaulaS <mariathecharmix@hotmail.com>
2020-06-01 21:29:33 -0500

    OnClicks para eventos añadidos

--- app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/LightRewardFragment.java
+++ app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/LightRewardFragment.java
@@ -17,7 +17,7 @@ import com.uniquindio.mueveteuq.R;
  * Use the {@link LightRewardFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LightRewardFragment extends Fragment {
+public class LightRewardFragment extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,6 +28,17 @@ public class LightRewardFragment extends Fragment {
     private String mParam2;
 
     private CardView cardViewManzana;
+    private CardView cardViewMandarina;
+    private CardView cardViewNaranja;
+    private CardView cardViewTomate;
+    private CardView cardViewMelon;
+    private CardView cardViewSandia;
+    private CardView cardViewUvas;
+    private CardView cardViewHuevo;
+    private CardView cardViewBanano;
+    private CardView cardViewCafe;
+    private CardView cardViewJugo;
+    private CardView cardViewAguaGas;
 
     public LightRewardFragment() {
         // Required empty public constructor
@@ -68,15 +79,38 @@ public class LightRewardFragment extends Fragment {
 
 
         cardViewManzana = view.findViewById(R.id.cardManzana);
+        cardViewMandarina = view.findViewById(R.id.cardMandarina);
+        cardViewNaranja = view.findViewById(R.id.cardNaranja);
+        cardViewTomate = view.findViewById(R.id.cardTomate);
+        cardViewMelon = view.findViewById(R.id.cardMelon);
+        cardViewSandia = view.findViewById(R.id.cardSandía);
+        cardViewUvas = view.findViewById(R.id.cardUvas);
+        cardViewHuevo = view.findViewById(R.id.cardHuevo);
+        cardViewBanano = view.findViewById(R.id.cardBanano);
+        cardViewCafe = view.findViewById(R.id.cardCafe);
+        cardViewJugo = view.findViewById(R.id.cardJugoFruta);
+        cardViewAguaGas = view.findViewById(R.id.cardAguaConGas);
 
-        cardViewManzana.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(), "Funciona", Toast.LENGTH_LONG).show();
-            }
-        });
+        cardViewManzana.setOnClickListener(this);
+        cardViewMandarina.setOnClickListener(this);
+        cardViewNaranja.setOnClickListener(this);
+        cardViewTomate.setOnClickListener(this);
+        cardViewMelon.setOnClickListener(this);
+        cardViewSandia.setOnClickListener(this);
+        cardViewUvas.setOnClickListener(this);
+        cardViewHuevo.setOnClickListener(this);
+        cardViewBanano.setOnClickListener(this);
+        cardViewCafe.setOnClickListener(this);
+        cardViewJugo.setOnClickListener(this);
+        cardViewAguaGas.setOnClickListener(this);
+
 
 
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/RewardsFragment.java
+++ app/src/main/java/com/uniquindio/mueveteuq/fragments/mainZone/RewardsFragment.java
@@ -17,7 +17,7 @@ import com.uniquindio.mueveteuq.R;
  * Use the {@link RewardsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RewardsFragment extends Fragment {
+public class RewardsFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -26,6 +26,20 @@ public class RewardsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private CardView cardViewHelado;
+    private CardView cardViewPaleta;
+    private CardView cardViewGaseosa;
+    private CardView cardViewChococake;
+    private CardView cardViewEspagueti;
+    private CardView cardViewPollo;
+    private CardView cardViewGelatina;
+    private CardView cardViewLemoncake;
+    private CardView cardViewHamburguesa;
+    private CardView cardViewPizza;
+    private CardView cardViewPapasfritas;
+    private CardView cardViewChocomilk;
+
 
 
 
@@ -66,7 +80,40 @@ public class RewardsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rewards, container, false);
 
+        cardViewHelado = view.findViewById(R.id.cardHeladoCrema);
+        cardViewPaleta = view.findViewById(R.id.cardPaleta);
+        cardViewGaseosa = view.findViewById(R.id.cardGaseosa);
+        cardViewChococake = view.findViewById(R.id.cardChocolateCake);
+        cardViewEspagueti = view.findViewById(R.id.cardEspaguetis);
+        cardViewPollo = view.findViewById(R.id.cardPolloAsado);
+        cardViewGelatina = view.findViewById(R.id.cardGelatina);
+        cardViewLemoncake = view.findViewById(R.id.cardTortaLimon);
+        cardViewHamburguesa = view.findViewById(R.id.cardHamburguesa);
+        cardViewPizza = view.findViewById(R.id.cardPizza);
+        cardViewPapasfritas = view.findViewById(R.id.cardPapasFritas);
+        cardViewChocomilk = view.findViewById(R.id.cardChocoMilk);
+
+
+
+        cardViewHelado.setOnClickListener(this);
+        cardViewPaleta.setOnClickListener(this);
+        cardViewGaseosa.setOnClickListener(this);
+        cardViewChococake.setOnClickListener(this);
+        cardViewEspagueti.setOnClickListener(this);
+        cardViewPollo.setOnClickListener(this);
+        cardViewGelatina.setOnClickListener(this);
+        cardViewLemoncake.setOnClickListener(this);
+        cardViewHamburguesa.setOnClickListener(this);
+        cardViewPizza.setOnClickListener(this);
+        cardViewPapasfritas.setOnClickListener(this);
+        cardViewChocomilk.setOnClickListener(this);
+
 
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
